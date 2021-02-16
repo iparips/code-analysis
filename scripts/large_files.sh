3fc3e109c88a56ea2e3dd2f5b4dd3509f8780ca2
Ilya Paripsa <iparips@gmail.com>
2021-02-17 09:54:30 +1100

    Apply ws suggestions to large files script

--- scripts/large_files.sh
+++ scripts/large_files.sh
@@ -3,13 +3,13 @@
 SIZE_THRESHOLD_KB="${1:-10}"
 for directory in `find ***REMOVED***/components -type d -depth 1`
 do
-    printf "\n-----------\nDirectory: $directory\n-----------\n"
-    num_of_files_in_dir=$(find $directory -type f | wc -l)
-    printf "Number of files: $num_of_files_in_dir\n"
-    large_files=$(find $directory -type f -name "*.js" ! -path "src/.next/*" -size +${SIZE_THRESHOLD_KB}k)
-    if [ ! -z "$large_files" ]
+    printf "\n-----------\nDirectory: %s\n-----------\n" "$directory"
+    num_of_files_in_dir=$(find "$directory" -type f | wc -l)
+    printf "Number of files: %s\n" "$num_of_files_in_dir"
+    large_files=$(find "$directory" -type f -name "*.js" ! -path "src/.next/*" -size +"${SIZE_THRESHOLD_KB}"k)
+    if [ -n "$large_files" ]
     then
       echo "Files larger than ${SIZE_THRESHOLD_KB}kb:"
-      cloc --list-file=$large_files
+      cloc --list-file="$large_files"
     fi
 done
