6bfcf6b3dbac8ef588b6a6375d2f21b6c991e706
Ilya Paripsa <iparips@gmail.com>
2021-02-17 17:31:00 +1100

    Make chart work with real data
    - only take data from components sub dir, because taking it from outside makes chart very slow

--- public/chart.js
+++ public/chart.js
@@ -1,7 +1,8 @@
-anychart.data.loadJsonFile("http://localhost:8080/pie_data.json", function (data) {
-  // create a chart and set loaded data
-  chart = anychart.bar(data);
-
+const request = new Request('http://localhost:8080/data/file_hierarchy_no_next.json');
+fetch(request).then( r => r.text()).then((data) => {
+  console.log("fetched data: ", JSON.parse(data));
+  const chart = anychart.treeMap(JSON.parse(data), "as-tree");
+  chart.maxDepth(3);
   chart.title("Load JSON data and create a chart");
   chart.container("container");
   chart.draw();
--- scripts/prepare_data.js
+++ scripts/prepare_data.js
@@ -1,8 +1,12 @@
-const dirTree = require("directory-tree");
+const dirTree = require('directory-tree');
 const fs = require('fs');
 
-const tree = dirTree("***REMOVED***src");
-fs.writeFile('data/file_hierarchy.json', JSON.stringify(tree, null, 2), function (err) {
+const sourceDir = '***REMOVED***'
+// const sourceDir = '***REMOVED***src'
+const outputPath = 'public/data/file_hierarchy_no_next.json'
+
+const tree = [dirTree(sourceDir, {exclude: /\.next/})];
+fs.writeFile(outputPath, JSON.stringify(tree, null, 2), function (err) {
   if (err) return console.log(err);
-  console.log('Writing finished.');
+  console.log(`Writing finished to ${outputPath}`);
 });
