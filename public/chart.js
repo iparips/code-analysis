b4b95cb24fd7fb6d2f5bc6439ef5ebd363ddd159
Ilya Paripsa <iparips@gmail.com>
2021-02-17 15:40:34 +1100

    Add remote loading of chart data

--- public/chart.js
+++ public/chart.js
@@ -1,15 +1,8 @@
-anychart.onDocumentLoad(function () {
-  // create an instance of a pie chart
-  const chart = anychart.pie();
-  // set the data
-  chart.data([
-    ["Chocolate", 5],
-    ["Rhubarb compote", 2],
-    ["Crêpe Suzette", 2],
-    ["American blueberry", 2],
-    ["Buttermilk", 1]
-  ]);
-  chart.title("Top 5 pancake fillings");
+anychart.data.loadJsonFile("http://localhost:8080/pie_data.json", function (data) {
+  // create a chart and set loaded data
+  chart = anychart.bar(data);
+
+  chart.title("Load JSON data and create a chart");
   chart.container("container");
   chart.draw();
 });
