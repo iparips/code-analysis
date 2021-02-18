29afcbcf195ac0d83b8a7021410edb311aadf790
Ilya Paripsa <iparips@gmail.com>
2021-02-18 12:45:21 +1100

    Add tooltip with branch coverage

--- public/chart.js
+++ public/chart.js
@@ -1,10 +1,18 @@
 const request = new Request('http://localhost:8080/data/file_hierarchy_no_next.json');
-fetch(request).then( r => r.text()).then((data) => {
+fetch(request).then(r => r.text()).then((data) => {
   console.log("fetched data: ", JSON.parse(data));
   const chart = anychart.treeMap(JSON.parse(data), "as-tree");
   chart.maxDepth(3);
   chart.hintDepth(1);
   chart.title("Load JSON data and create a chart");
   chart.container("container");
+
+  chart.tooltip().format(function () {
+    const size = "Size: " + this.value + " LoC"
+    const branchCoverage = this.getData("branchCoverage") ?
+      "\nBranch Coverage: " + this.getData("branchCoverage") : ""
+    return size + branchCoverage
+  });
+
   chart.draw();
 });
