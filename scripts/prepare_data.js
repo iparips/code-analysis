28dc40578c391b5e068d11fa1614aafd73b5751b
Ilya Paripsa <iparips@gmail.com>
2021-02-18 12:01:32 +1100

    Add branch coverage
    Bump up node version to allow for option chaining

--- scripts/prepare_data.js
+++ scripts/prepare_data.js
@@ -2,13 +2,16 @@ const dirTree = require('directory-tree');
 const fs = require('fs');
 const {execSync} = require('child_process')
 
+const coveragePath = '***REMOVED***coverage/coverage-summary.json'
+const coverageJson = JSON.parse(fs.readFileSync(coveragePath).toString())
+
 const sourceDir = '***REMOVED***'
-// const sourceDir = '***REMOVED***src'
 const outputPath = 'public/data/file_hierarchy_no_next.json'
 
 const tree = dirTree(sourceDir, {exclude: /\.next/}, (file, path, stats) => {
   const numberOfLines = execSync(`cat ${file.path} | sed '/^\\s*$/d' | wc -l`).toString()
   file.value = numberOfLines
+  file.branchCoverage = coverageJson[file.path]?.branches?.pct
 });
 
 const treeArray = [tree]
