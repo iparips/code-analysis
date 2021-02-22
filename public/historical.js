d011f87cb7e6e7c34f7c8e310b57cd18e7be5c44
Ilya Paripsa <iparips@gmail.com>
2021-02-22 18:52:02 +1100

    Prep for having multiple lines

--- public/historical.js
+++ public/historical.js
@@ -2,7 +2,9 @@ const request = new Request('http://localhost:8080/data/historical_ratios.json')
 fetch(request).then(r => r.text()).then((dataStr) => {
   const data = JSON.parse(dataStr);
   console.log(data);
-  const chart = anychart.line(data);
+  const chart = anychart.line();
+  chart.defaultSeriesType("line")
+  chart.addSeries(data)
   chart.title("Ratio of test code to application code");
   chart.container("container");
   chart.draw();
